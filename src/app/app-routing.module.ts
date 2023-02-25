158b75dfe621bc64e298729b3100552ee380a0ba
wandersonwandao@hotmail.com <wandersonwandao@hotmail.com>
2023-02-24 23:22:15 -0300

    Rota para o Componente Home

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,13 @@
+import { HomeComponent } from './components/home/home.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
-    path: '',component:NavbarComponent
+    path: '',component:NavbarComponent,children:[
+      {path: 'home',component: HomeComponent}
+    ]
   }
 ];
 
