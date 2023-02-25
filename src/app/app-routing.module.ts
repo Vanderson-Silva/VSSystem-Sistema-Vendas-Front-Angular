3d74f31103a3b7f28cd61d03eeb2cb5f1c9ca9ec
wandersonwandao@hotmail.com <wandersonwandao@hotmail.com>
2023-02-24 23:09:34 -0300

    HTML e CSS para o componente navbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,8 +4,7 @@ import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
-    path: '',
-    component:NavbarComponent
+    path: '',component:NavbarComponent
   }
 ];
 
