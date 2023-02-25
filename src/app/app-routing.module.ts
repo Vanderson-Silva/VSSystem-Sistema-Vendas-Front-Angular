f12891aeb7a25e74af36aa995768de7c06bbf991
wandersonwandao@hotmail.com <wandersonwandao@hotmail.com>
2023-02-24 22:20:25 -0300

    Rota para componente Navbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
+import { NavbarComponent } from './components/navbar/navbar.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component:NavbarComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
