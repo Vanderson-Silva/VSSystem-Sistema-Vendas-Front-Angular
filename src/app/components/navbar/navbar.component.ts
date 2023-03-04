82127a3bda047a4511f951e6428373f1f1284cdc
wandersonwandao@hotmail.com <wandersonwandao@hotmail.com>
2023-03-04 17:15:22 -0300

    Logout no Sistema

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,3 +1,5 @@
+import { ToastrService } from 'ngx-toastr';
+import { AuthService } from './../../services/auth.service';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
@@ -8,11 +10,17 @@ import { Router } from '@angular/router';
 })
 export class NavbarComponent  implements OnInit{
 
-  constructor(private router:Router){}
+  constructor(private router:Router, private authService: AuthService, private toast: ToastrService){}
 
   ngOnInit(): void {
     this.router.navigate([''])
     
+  }
+
+  logout(){
+    this.router.navigate(['login'])
+    this.authService.logout();
+      this.toast.info('Logout Realizado com Sucesso','Logout',{timeOut: 7000})
   }
 
 }
